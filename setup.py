eb806a33cac75379b8e8ce4c08aad3e9bf929acc
Daniel Lima <dnlima77237@gmail.com>
2024-08-26 08:31:20 -0300

    Created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="passgen",
+    version="0.1.1",
+    description="A cli password generator made with python",
+    url="https://github.com/limadaniel70/passgen",
+    author="Daniel Lima",
+    license="MIT"
+)
